e9566d535973d096624d3911e8b20eb1016b1203
Carlo Giustozzi <carlog1118@gmail.com>
2020-11-03 21:45:04 -0500

    added comments for mailchimp emailjs

--- src/ContactPage/ContactPage.js
+++ src/ContactPage/ContactPage.js
@@ -1,36 +1,48 @@
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import emailjs from "emailjs-com";
 import "./ContactPage.css";
 
 class ContactPage extends React.Component {
+
+  /*Hello tiff,
+
+You got a new message {{from_name}}:
+
+{{from_email}}
+
+{{message}}
+
+Best wishes,
+EmailJS team*/ 
+
+  sendEmail = (e) => {
+    e.preventDefault() 
+    //(service_id, template_id, e_target, user_id)
+    emailjs.sendForm('service_dtwjim6', 'template_k4jv3x9', e.target, 'user_YvYoxq48ilj781xjFyTkO')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text)
+      });
+  }
+ 
   render() {
     return (
       <div className="contact-page-cont">
         <Header />
         <section className="contact-page">
           <h2>Let's Talk</h2>
-          <form
-            className="email-form"
-            action="mailto:someone@example.com"
-            method="post"
-            encType="text/plain"
-          >
-            Name:
-            <br />
-            <input type="text" name="name" />
-            <br />
-            E-mail:
-            <br />
-            <input type="text" name="mail" />
-            <br />
-            Comment:
-            <br />
-            <input type="text" name="comment" size="50" />
-            <br />
-            <br />
+          <form className="contact-form" >
+            <input type="hidden" name="contact_number" />
+            <label>Name</label>
+            <input type="text" name="from_name" />
+            <label>Email</label>
+            <input type="email" name="from_email" />
+            <label>Message</label>
+            <textarea name="message" />
             <input type="submit" value="Send" />
-            <input type="reset" value="Reset" />
           </form>
         </section>
         <Footer />
--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,10 +3,13 @@ import MailchimpSubscribe from "react-mailchimp-subscribe";
 
 import "./Footer.css";
 
+/* create new, signup form, begin, action="" */
+
 class Footer extends React.Component {
   render() {
     return (
       <footer>
+        <p>Subscribe to Newsletter</p>
         <MailchimpSubscribe url="https://gmail.us17.list-manage.com/subscribe/post?u=282d8fce867cb528332793893&amp;id=bc6a6da0df" />
         <div className="social-media">
           <div className="icon">LI</div>
